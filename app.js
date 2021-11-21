93d7cf3f8dac4e88eabec1c533fc1a736ee1f582
jaedeokhan <hjaedeok@gmail.com>
2021-11-21 16:46:37 +0900

    [ADD] - 4.6 Loading Username - 브라우저의 localStorage를 통해서 먼저 진행해봄 - localStorage의 CRUD API인 setItem(), getItem(), removeItem() 등이   있다.

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ const HIDDEN_CLASSNAME = "hidden";
 function onLoginFormSubmit(event){
     event.preventDefault();
     const username = loginInput.value;
+    localStorage.setItem("username", username);
 
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    console.log(username);
 
     greeting.classList.remove(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hello ${username} keep going`;
+    greeting.innerText = `Hello ${localStorage.getItem("username")} keep going`;
+};
+
+function checkLoginUser(){
+    const loginUser = localStorage.getItem("username");
+
+    if (loginUser !== null){
+        loginForm.classList.add(HIDDEN_CLASSNAME);
+        greeting.classList.remove(HIDDEN_CLASSNAME);
+        greeting.innerText = `Hello ${loginUser} keep going`;
+    }
 };
 
 loginForm.addEventListener("submit", onLoginFormSubmit);
+window.addEventListener("load", checkLoginUser);
