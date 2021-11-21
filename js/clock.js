570b9fd44141bafc17625f12db32a1b488aaec9d
jaedeokhan <hjaedeok@gmail.com>
2021-11-21 18:19:14 +0900

    [ADD] 5.1 Timeouts and Dates - setTimeout() : 몇 초후에 한 번 동작하게 하는 함수

--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,22 @@
 
 const clock = document.querySelector("h2#clock");
 
-// intervals(간격, 몇 초마다), timeout
+// intervals(간격, 몇 초마다), timeout(몇 초후에)
 
-function sayHello(){
-    console.log("hello");
+function fillZero(time){
+    return (time < 10) ? `0${time}` : time;
 }
 
-setInterval(sayHello, 5000);
+function getClock(){
+    const date = new Date();
+    const hour = fillZero(date.getHours());
+    const minutes = fillZero(date.getMinutes());
+    const seconds = fillZero(date.getSeconds());
+
+    const currentTime = `${hour}:${minutes}:${seconds}`
+    
+    clock.innerText = currentTime;
+}
+
+getClock();
+setInterval(getClock, 1000);
