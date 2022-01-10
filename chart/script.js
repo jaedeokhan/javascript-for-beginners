e5cf40a3bc1338dc0da264391fff5d10d627da3c
jaedeokhan <hjaedeok@gmail.com>
2022-01-10 23:33:43 +0900

    [ADD] chart.js bar 바로 위에 데이터 찍기

--- chart/script.js
+++ chart/script.js
@@ -6,12 +6,12 @@ var months = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9
 var orcDatas = [
     8500, 6367, 7000, 6900, 8000,
     6367, 11240, 7000, 5000, 6000,
-    7000, 12000
+    7000, 0
 ];
 var percentDatas = [
-    80, 79, 78, 90, 100,
+    80, 79, 78, 90, 120,
     83, 81, 80, 78, 83, 
-    81, 100
+    81, 0
 ];
 
 var myChart = new Chart(ctx, {
@@ -34,7 +34,7 @@ var myChart = new Chart(ctx, {
       data: percentDatas,
       fill: false 
     }]
-  },
+  }, // End of data tag
   options: {
     legend : {
         position : "bottom"
@@ -47,7 +47,7 @@ var myChart = new Chart(ctx, {
         gridLines : {
             display : false // x축의 grid 라인 안 보이게
         }
-      }],
+      }], // End of xAxes 
       yAxes: [{ 
         id: 'ORC',
         position: 'left',
@@ -60,14 +60,15 @@ var myChart = new Chart(ctx, {
         },
         ticks: {
           min: 0,
-          //max: 100,
+          max: 20000,
           stepSize: 5000,
           //fontColor: '#ffbaa2',
           callback: function(value, index, values) {
             return value + '억 원';
           }
         }
-      }, {
+      }, // End Of yAxes ID ORC
+      {
         id: 'OpRWA',
         type: 'linear',
         position: 'right',
@@ -77,14 +78,14 @@ var myChart = new Chart(ctx, {
         },
         ticks: {
           min: 0,
-          max: 100,
+          //max: 100,
           stepSize: 25,
           //fontColor: '#c881d2',
           callback: function(value, index, values) {
             return value + "%";
           }
         }
-      }]
+      }] // End Of yAxes ID OpRWA
     },
     elements: {
       line: {
@@ -95,23 +96,42 @@ var myChart = new Chart(ctx, {
         borderWidth: 1,
         //pointStyle: 'circle'
       }, 
+    }, // End of elements 
+    hover : {
+      animationDuration : 0 // 0으로 하면 hover 하지 않아도 계속 보이게
     },
-    // animation : {
-    //   duration : 1,
-    //   onComplete: function () {
-    //     var chartInstance = this.chart,
-    //       ctx = chartInstance.ctx;
-    //       ctx.textAlign = 'center';
-    //       ctx.fillStyle = "rgba(0, 0, 0, 1)";
-    //       ctx.textBaseline = 'bottom';
-    //       this.data.datasets.forEach(function (dataset, i) {
-    //         var meta = chartInstance.controller.getDatasetMeta(i);
-    //         meta.data.forEach(function (bar, index) {
-    //           var data = dataset.data[index];
-    //           ctx.fillText(data, bar._model.x, bar._model.y - 5);
-    //         });
-    //       });
-    //     }
-    // }
-  }
+    animation : {
+      duration : 1,
+      onComplete : function(){
+            var chartInstance = this.chart;
+            ctx = chartInstance.ctx;
+
+            ctx.textAlign = "center";
+            ctx.textBaseline = "bottom";
+            ctx.fillStyle = "black";
+
+            this.data.datasets.forEach(function(dataset, i){
+            var meta = chartInstance.controller.getDatasetMeta(i);
+
+            console.log(dataset);
+
+            meta.data.forEach(function(bar, index){
+            var data = dataset.data[index];
+                      
+            // line bar이면
+            if (dataset.type == "line"){ 
+              data = data + "%"; // 뒤에 % 표시
+            }
+            
+            // data가 0이거나 0%이면 data를 공백으로
+            if (data == 0 || data == "0%"){
+              data = "";
+            }
+
+            ctx.fillText(data, bar._model.x, bar._model.y -5); // bar에 값 표시
+          }); // End of this.data.datasets for loop
+        });
+      } // End of onComplete function
+    } // End of animation
+  } // End of options 
 });
